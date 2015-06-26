ff766c788c750c50a71bf2544a922e8e8b61b6d0
elfnor <elfnor@gmail.com>
2015-06-26 15:24:37 +1200

    before refactoring into process_statement function

--- nodes/generator/generative_art.py
+++ nodes/generator/generative_art.py
@@ -25,8 +25,8 @@ import bpy
 from bpy.props import BoolProperty, IntProperty, StringProperty
 
 from sverchok.node_tree import SverchCustomTreeNode
-from sverchok.data_structure import (changable_sockets, multi_socket, 
-                            SvSetSocketAnyType, SvGetSocketAnyType, updateNode, 
+from sverchok.data_structure import (changable_sockets, multi_socket,
+                            SvSetSocketAnyType, SvGetSocketAnyType, updateNode,
                             fullList, Vector_generate, Matrix_listing)
 
 import random
@@ -37,7 +37,6 @@ import xml.etree.cElementTree as etree
 from xml.etree.cElementTree import fromstring
 
 
-
 def flat(*args):
     for x in args:
         if hasattr(x, '__iter__'):
@@ -45,15 +44,15 @@ def flat(*args):
                 yield y
         else:
             yield x
-            
-"""   
+
+"""
 ---------------------------------------------------
     LSystem
 ---------------------------------------------------
-"""            
-            
-class LSystem:
+"""
 
+
+class LSystem:
     """
     Takes an XML string.
     """
@@ -62,62 +61,51 @@ class LSystem:
         self._maxDepth = int(self._tree.get("max_depth"))
         self._voxelSize = self._tree.get("clearance")
         logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)-8s %(filename)s:%(lineno)-4d: %(message)s')
-        logging.info("clearance %s" %(self._voxelSize))
-       
+             format='%(levelname)-8s %(filename)s:%(lineno)-4d: %(message)s')
+        logging.info("clearance %s" % (self._voxelSize))
+
         self._progressCount = 0
         self._maxObjects = maxObjects
-        
-        
 
     """
     Returns a list of "shapes".
     Each shape is a 2-tuple: (shape name, transform matrix).
     """
-    def evaluate(self, seed = 0 ):
+    def evaluate(self, seed=0):
         random.seed(seed)
         rule = _pickRule(self._tree, "entry")
         entry = (rule, 0, mu.Matrix.Identity(4))
         shapes = self._evaluate(entry)
         return shapes
-        
+
     def _evaluate(self, entry):
         stack = [entry]
         shapes = []
         locs = set()
         voxel_stop = False
+        last_loc_key = None
         while len(stack) > 0:
-            
+
             if len(shapes) > self._maxObjects:
-                print('max objects reached', len(shapes), self._maxObjects)
-                break
-                
-            if voxel_stop:
-                if "successor" in rule.attrib:
-                    successor = rule.get("successor")
-                    rule = _pickRule(self._tree, successor)
-                    stack.append((rule, 0, matrix))
-                shapes.append(None)
-                voxel_stop = False
-                continue      
-    
+                print(('max objects reached', len(shapes), self._maxObjects))
+                break # out of while len(stack) > 0: loop
+
             if len(shapes) > self._progressCount + 1000:
-                print(len(shapes), "curve segments so far")
-                print(self._maxObjects)
+                print((len(shapes), "curve segments so far"))
+                print((self._maxObjects))
                 self._progressCount = len(shapes)
-                
-            
+
             rule, depth, matrix = stack.pop()
 
             local_max_depth = self._maxDepth
             if "max_depth" in rule.attrib:
                 local_max_depth = int(rule.get("max_depth"))
-    
+
             if len(stack) >= self._maxDepth:
                 shapes.append(None)
                 logging.info('shape len(stack) >= self._maxDepth None')
-                continue
-    
+                continue # with next iteration of while len(stack) > 0: loop
+
             if depth >= local_max_depth:
                 if "successor" in rule.attrib:
                     successor = rule.get("successor")
@@ -125,69 +113,95 @@ class LSystem:
                     stack.append((rule, 0, matrix))
                 shapes.append(None)
                 logging.info('shape depth >= local_max_depth None')
+                continue  # with next iteration of while len(stack) > 0: loop
+
+            if voxel_stop:
+                if "successor" in rule.attrib:
+                    successor = rule.get("successor")
+                    rule = _pickRule(self._tree, successor)
+                    stack.append((rule, 0, matrix))
+                shapes.append(None)
+                voxel_stop = False
                 continue
-                
-              
-        
-            for statement in rule:              
-                tstr = statement.get("transforms","")
+
+            for statement in rule:
+                tstr = statement.get("transforms", "")
                 if not(tstr):
                     tstr = ''
-                    for t in ['tx', 'ty', 'tz', 'rx', 'ry', 'rz', 'sa', 'sx', 'sy', 'sz']:
+                    for t in ['tx', 'ty', 'tz', 'rx', 'ry', 'rz',
+                              'sa', 'sx', 'sy', 'sz']:
                         tvalue = statement.get(t)
                         if tvalue:
                             n = eval(tvalue)
-                            tstr += "{} {:f} ".format(t,n) 
+                            tstr += "{} {:f} ".format(t, n)
                 xform = _parseXform(tstr)
                 count = int(statement.get("count", 1))
                 for n in range(count):
                     matrix *= xform
-                    
+
                     if statement.tag == "call":
                         rule = _pickRule(self._tree, statement.get("rule"))
                         cloned_matrix = matrix.copy()
-                        entry = (rule, depth + 1, cloned_matrix)     
+                        entry = (rule, depth + 1, cloned_matrix)
                         stack.append(entry)
-                  
+
                     elif statement.tag == "instance":
+                        vecTrans = matrix.to_translation()
                         if self._voxelSize:
-                            loc_key = tuple([round(i) for i in matrix.to_translation()/float(self._voxelSize)])
+                            loc_key = tuple(
+                                      [round(i)
+                                       for i
+                                       in vecTrans / float(self._voxelSize)])
                         else:
-                            loc_key = (0,0,0)
-                            
+                            loc_key = (0, 0, 0)
+
                         #print(loc_key, len(shapes))
-                        if loc_key not in locs:
+                        if loc_key in locs and loc_key != last_loc_key:
+                            # at this point we are three deep in loops
+                            # while len(stack) > 0:
+                            #    ....    
+                            #    for statement in rule:
+                            #        ....
+                            #        for n in range(count): 
+                            #            ....
+                            # and need to go back to next iteration of while loop
+
+                            voxel_stop = True
+                            logging.info('{0} voxel_stop {1} {2}'.format(loc_key, tstr, vecTrans))
+                            logging.info(len(stack))
+                            if len(stack) > 1:
+                                logging.info(stack[-1][0].get("dir"))
+                                logging.info(stack[-2][0].get("dir"))
+                        else:
                             if self._voxelSize:
                                 locs.add(loc_key)
+                                last_loc_key = loc_key
                             name = statement.get("shape")
                             shape = (name, matrix)
                             shapes.append(shape)
-                            logging.info('{0} shape {1} {2} {3}'.format(loc_key, name, tstr, matrix.to_translation()))
+                            logging.info('{0} shape {1} {2} {3}'.format(loc_key, name, tstr, vecTrans))
                             voxel_stop = False
-                        else:
-                            voxel_stop = True  
-                            logging.info('{0} voxel_stop {1} {2}'.format(loc_key, tstr, matrix.to_translation()))
-                                                  
+
                     else:
                         raise ValueError("bad xml", statement.tag)
 
-    
-        print("\nGenerated %d shapes." % len(shapes))
+        print(("\nGenerated %d shapes." % len(shapes)))
         return shapes
         # end of _evaluate
-        
+
     def make_tube(self, mats, verts):
         """
         takes a list of vertices and a list of matrices
-        the vertices are to be joined in a ring, copied and transformed by the 1st matrix 
+        the vertices are to be joined in a ring,
+        copied and transformed by the 1st matrix
         and this ring joined to the previous ring.
 
         The ring dosen't have to be planar.
         outputs lists of vertices, edges and faces
         """
-        
+
         edges_out = []
-        verts_out = [] 
+        verts_out = []
         faces_out = []
         vID = 0
         if len(mats) > 1:
@@ -196,32 +210,38 @@ class LSystem:
             nring = len(verts[0])
             #end face
             faces_out.append(list(range(nring)))
-            for i,m in enumerate(mats):
-                for j,v in enumerate(verts[0]):
+            for i, m in enumerate(mats):
+                for j, v in enumerate(verts[0]):
                     vout = mu.Matrix(m) * mu.Vector(v)
                     verts_out.append(vout.to_tuple())
-                    vID = j + i*nring
+                    vID = j + i * nring
                     #rings
                     if j != 0:
                         edges_out.append([vID, vID - 1])
-                    else: 
-                        edges_out.append([vID, vID + nring-1]) 
+                    else:
+                        edges_out.append([vID, vID + nring - 1])
                     #lines
                     if i != 0:
-                        edges_out.append([vID, vID - nring]) 
+                        edges_out.append([vID, vID - nring])
                         #faces
                         if j != 0:
-                            faces_out.append([vID, vID - nring, vID - nring - 1, vID-1,])
+                            faces_out.append([vID,
+                                              vID - nring,
+                                              vID - nring - 1,
+                                              vID - 1])
                         else:
-                            faces_out.append([vID, vID - nring,  vID-1, vID + nring-1])
+                            faces_out.append([vID,
+                                              vID - nring,
+                                              vID - 1,
+                                              vID + nring - 1])
             #end face
             #reversing list fixes face normal direction keeps mesh manifold
-            f = list(range(vID, vID-nring, -1))
+            f = list(range(vID, vID - nring, - 1))
             faces_out.append(f)
         return verts_out, edges_out, faces_out
-                            
-def _pickRule(tree, name):
 
+
+def _pickRule(tree, name):
     rules = tree.findall("rule")
     elements = []
     for r in rules:
@@ -229,7 +249,7 @@ def _pickRule(tree, name):
             elements.append(r)
 
     if len(elements) == 0:
-        raise ValueError("bad xml",  "no rules found with name '%s'" % name)
+        raise ValueError("bad xml", "no rules found with name '%s'" % name)
 
     sum, tuples = 0, []
     for e in elements:
@@ -245,16 +265,17 @@ def _pickRule(tree, name):
 
 _xformCache = {}
 
+
 def _parseXform(xform_string):
     if xform_string in _xformCache:
         return _xformCache[xform_string]
-        
+
     matrix = mu.Matrix.Identity(4)
     tokens = xform_string.split(' ')
     t = 0
     while t < len(tokens) - 1:
             command, t = tokens[t], t + 1
-    
+
             # Translation
             if command == 'tx':
                 x, t = eval(tokens[t]), t + 1
@@ -270,19 +291,19 @@ def _parseXform(xform_string):
                 y, t = eval(tokens[t]), t + 1
                 z, t = eval(tokens[t]), t + 1
                 matrix *= mu.Matrix.Translation(mu.Vector((x, y, z)))
-    
+
             # Rotation
             elif command == 'rx':
                 theta, t = _radians(eval(tokens[t])), t + 1
                 matrix *= mu.Matrix.Rotation(theta, 4, 'X')
-                
+
             elif command == 'ry':
                 theta, t = _radians(eval(tokens[t])), t + 1
                 matrix *= mu.Matrix.Rotation(theta, 4, 'Y')
             elif command == 'rz':
                 theta, t = _radians(eval(tokens[t])), t + 1
                 matrix *= mu.Matrix.Rotation(theta, 4, 'Z')
-    
+
             # Scale
             elif command == 'sx':
                 x, t = eval(tokens[t]), t + 1
@@ -303,7 +324,7 @@ def _parseXform(xform_string):
                 mx = mu.Matrix.Scale(x, 4, mu.Vector((1.0, 0.0, 0.0)))
                 my = mu.Matrix.Scale(y, 4, mu.Vector((0.0, 1.0, 0.0)))
                 mz = mu.Matrix.Scale(z, 4, mu.Vector((0.0, 0.0, 1.0)))
-                mxyz = mx*my*mz
+                mxyz = mx * my * mz
                 matrix *= mxyz
 
             else:
@@ -312,14 +333,16 @@ def _parseXform(xform_string):
     _xformCache[xform_string] = matrix
     return matrix
 
+
 def _radians(d):
     return float(d * 3.141 / 180.0)
-    
-"""   
+
+"""
 ---------------------------------------------------
     SvGenerativeArtNode
 ---------------------------------------------------
 """
+
 
 class SvGenerativeArtNode(bpy.types.Node, SverchCustomTreeNode):
     ''' Generative Art or LSystem node'''
@@ -333,7 +356,8 @@ class SvGenerativeArtNode(bpy.types.Node, SverchCustomTreeNode):
                        default=21, min=0, options={'ANIMATABLE'},
                        update=updateNode)
 
-    maxmats = IntProperty(name='maxmats', description='maximum nunber of matrices',
+    maxmats = IntProperty(name='maxmats',
+                       description='maximum nunber of matrices',
                        default=1000, min=1, options={'ANIMATABLE'},
                        update=updateNode)
 
@@ -346,10 +370,10 @@ class SvGenerativeArtNode(bpy.types.Node, SverchCustomTreeNode):
     multi_socket_type = 'StringsSocket'
 
     def draw_buttons(self, context, layout):
-        layout.prop_search(self, 'filename', bpy.data, 'texts', text='', icon='TEXT') 
+        layout.prop_search(self, 'filename', bpy.data,
+                          'texts', text='', icon='TEXT')
         layout.prop(self, "rseed", text="r seed")
         layout.prop(self, "maxmats", text="max mats")
-      
 
     def sv_init(self, context):
         self.inputs.new('VerticesSocket', "Vertices")
@@ -357,11 +381,10 @@ class SvGenerativeArtNode(bpy.types.Node, SverchCustomTreeNode):
 
         self.outputs.new('MatrixSocket', "Matrices")
         self.outputs.new('StringsSocket', "Mask")
-        
+
         self.outputs.new('VerticesSocket', "Vertices")
         self.outputs.new('StringsSocket', "Edges")
         self.outputs.new('StringsSocket', "Faces")
-
 
     def update(self):
         # inputs
@@ -376,52 +399,52 @@ class SvGenerativeArtNode(bpy.types.Node, SverchCustomTreeNode):
     def process(self):
         if not self.filename:
             return
-        #xml text must be an internal file     
+        #xml text must be an internal file
         if not (self.filename in bpy.data.texts):
             return
         internal_file = bpy.data.texts[self.filename]
-        self.xml_text = internal_file.as_string()     
-        #nvars = len(set([name for text, name, spec, conv in string.Formatter().parse(self.xml_text)]))    
-        
-        nvars = self.xml_text.count('{') #this may be too large because of repeats        
-        
+        self.xml_text = internal_file.as_string()
+        #nvars may be too large because of repeats
+        nvars = self.xml_text.count('{')
+
         if ((self.outputs['Matrices'].is_linked) or (self.outputs['Vertices'].is_linked)):
             slots = []
             for socket in self.inputs[1:]:
                 if socket.is_linked:
                     slots.append(SvGetSocketAnyType(self, socket))
             #flatten vars
-            if slots: 
+            if slots:
                 slots = list(flat(slots))
                 fullList(slots, nvars)
-            else: 
-                slots = [0]*nvars
+            else:
+                slots = [0] * nvars
 
             self.xml_text = self.xml_text.format(*slots)
 
             lsys = LSystem(self.xml_text, self.maxmats)
-            shapes = lsys.evaluate(seed = self.rseed)
-       
+            shapes = lsys.evaluate(seed=self.rseed)
+
             names = [shape[0] for shape in shapes if shape]
             #convert names to integer list
-            iddict = {k:v for v,k in enumerate( sorted( set(names) ) )} 
-            
+            iddict = {k: v for v, k in enumerate(sorted(set(names)))}
+
             mat_sublist = []
             mat_list = []
             mask_list = []
-            
+
             edges_out = []
-            verts_out = [] 
-            faces_out = [] 
-            #make last entry in shapes None to allow make tube to finish last tube
+            verts_out = []
+            faces_out = []
+            #make last entry in shapes None
+            #to allow make tube to finish last tube
             if shapes[-1]:
-                shapes.append(None)            
+                shapes.append(None)
             for i, shape in enumerate(shapes):
                 if shape:
                     mat_sublist.append(shape[1])
                     mat_list.append(shape[1])
                     mask_list.append(iddict[shape[0]])
-                else: 
+                else:
                     if len(mat_sublist) > 0:
                         if self.inputs['Vertices'].is_linked:
                             verts = Vector_generate(SvGetSocketAnyType(self, self.inputs['Vertices']))
@@ -430,14 +453,14 @@ class SvGenerativeArtNode(bpy.types.Node, SverchCustomTreeNode):
                                 verts_out.append(v)
                                 edges_out.append(e)
                                 faces_out.append(f)
-                            
+
                     mat_sublist = []
-                    
+
             if self.outputs['Matrices'].is_linked:
                 SvSetSocketAnyType(self, 'Matrices', Matrix_listing(mat_list))
             if self.outputs['Mask'].is_linked:
                 SvSetSocketAnyType(self, 'Mask', [mask_list])
-                
+
             if self.outputs['Vertices'].is_linked:
                 SvSetSocketAnyType(self, 'Vertices', verts_out)
             if self.outputs['Edges'].is_linked:
